2949d260729b01813500ce470a4ebfb41b23a6d3
Zarahhhh <zakariyyahrahmat@gmail.com>
2025-03-08 15:56:21 +0100

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
+  content => "
+Host server01
+    HostName 18.204.215.41
+    IdentityFile ~/.ssh/school
+    User ubuntu
+    PasswordAuthentication no
+",
+}
